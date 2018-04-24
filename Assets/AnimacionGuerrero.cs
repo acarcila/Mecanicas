4638dec5141f70aba36d398d34cc9aac6a91f8b1
Alejandro Garzón <cagg40@hotmail.com>
2018-04-24 17:46:14 -0500

    Agregado modelo alfa, beta, reeeeeeeeeee beta del guerrero con animaciones

---
+++ Assets/AnimacionGuerrero.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacionGuerrero : MonoBehaviour {
+
+	public Animator anim;
+	private MovimientoPersonaje personaje;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		personaje = GetComponentInParent<MovimientoPersonaje> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(personaje.mover)
+		{
+			anim.Play ("Walk");
+		}else{
+			anim.Play ("Idle");
+		}
+	}
+}

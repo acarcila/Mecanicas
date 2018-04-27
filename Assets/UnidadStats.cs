5a543c50becbbe59e6520078102dce5e99a6366e
Alejandro Garzón <cagg40@hotmail.com>
2018-04-26 21:13:34 -0500

    Health bar added

--- Assets/UnidadStats.cs
+++ Assets/UnidadStats.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnidadStats : MonoBehaviour {
 
@@ -10,14 +11,18 @@ public class UnidadStats : MonoBehaviour {
 	public int movimiento;
 	public int rango;
 	public string unidadEnemiga;
+	public Image healthBar;
+
+	private float vidaInicial;
 
 	// Use this for initialization
 	void Start () {
-		
+		vidaInicial = vida;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		healthBar.fillAmount = vida / vidaInicial;
 		if(vida < 0)
 		{
 			muerte();

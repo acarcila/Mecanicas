efcdcee8db34bb85d45e495cef81dfd39cd4521f
Alejandro Garzón <cagg40@hotmail.com>
2018-05-05 16:55:36 -0500

    Game over scene added

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -6,16 +6,22 @@ using UnityEngine.SceneManagement;
 public class Main : MonoBehaviour {
 
 	private UnidadStats vidaCastilloRojo,vidaCastilloAzul;
-
+	private TiempoControlador tiempo;
+	private ConocerGanador ganador;
 
 	void Start () {
 		vidaCastilloRojo = GameObject.Find("Castillo Rojo").GetComponent<UnidadStats>();
 		vidaCastilloAzul = GameObject.Find("Castillo Azul").GetComponent<UnidadStats>();
+		ganador = GameObject.Find("Ganador").GetComponent<ConocerGanador>();
+		tiempo = GameObject.Find("Time").GetComponent<TiempoControlador>();
 	}
-	
+		
 	// Update is called once per frame
 	void Update () {
-		
+		if(tiempo.tiempo <= 0){
+			ganador.bandera = true;
+			cambiarPantalla();
+		}
 	}
 
 	public string compararVida(){
@@ -27,5 +33,9 @@ public class Main : MonoBehaviour {
 			 ganador = "Azul";
 		}
 		return ganador;
+	}
+
+	public void cambiarPantalla(){
+		SceneManager.LoadScene ("Fin");
 	}
 }

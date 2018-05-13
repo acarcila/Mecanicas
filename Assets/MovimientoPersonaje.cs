8c486f19129346d17159a24f79fc099e1c4cdc89
Alejandro Garzón <cagg40@hotmail.com>
2018-05-13 12:05:52 -0500

    Castle added

--- Assets/MovimientoPersonaje.cs
+++ Assets/MovimientoPersonaje.cs
@@ -25,21 +25,18 @@ public class MovimientoPersonaje : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		vectorPosicion = meta.position;
-
-		if(meta.name == "Castillo Rojo" || meta.name == "Castillo Azul")
-		{
-			vectorPosicion = new Vector3 (transform.position.x, transform.position.y, meta.transform.position.z);
+		if(this.name != "Castillo Rojo" && this.name != "Castillo Azul"){
+			if(meta.name == "Castillo Rojo" || meta.name == "Castillo Azul")
+				{
+					vectorPosicion = new Vector3 (transform.position.x, transform.position.y, meta.transform.position.z);
+				}
+				if(mover)
+				{
+					transform.position = Vector3.MoveTowards (transform.position, vectorPosicion, personaje.movimiento * Time.deltaTime);		
+					transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+					transform.LookAt(vectorPosicion);
+					transform.Rotate (0,180f,0);
+				}
 		}
-
-		if(mover)
-		{
-//			transform.position = Vector3.MoveTowards (transform.position, meta.position, personaje.movimiento * Time.deltaTime);		
-			transform.position = Vector3.MoveTowards (transform.position, vectorPosicion, personaje.movimiento * Time.deltaTime);		
-			transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
-			transform.LookAt(vectorPosicion);
-			transform.Rotate (0,180f,0);
-		}
-
-		
 	}
 }
